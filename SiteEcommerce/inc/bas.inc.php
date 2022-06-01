b93d6fe1c3035b34551f7b6af7d1e2be0cb59b5b
aklidif <difakli049@gmail.com>
2022-06-01 09:40:32 +0200

    update

--- SiteEcommerce/inc/bas.inc.php
+++
@@ -1,9 +0,0 @@
-			</div>
-        </section>
-        <footer>
-			<div class="conteneur">    
-				<?php echo date('Y'); ?> - Tous droits reservés.
-			</div>
-        </footer>
-    </body>
-</html>
